9ad054f052a13037ad9f2954e3552a55b7a1f8a2
Mateus S. Neubarth <69695735+MateusSNeubarth@users.noreply.github.com>
2024-10-09 14:52:18 +0000

    FEAT: bus/ticket classes; created basic schema

--- src/main/java/busticket/Bus.java
+++ src/main/java/busticket/Bus.java
@@ -1,8 +1,25 @@
 package busticket;
 
 public class Bus {
+    private int id;
 
-    public static void main(String[] args) {
-        System.out.println("Bus Test");
+    // Check if the card is valid
+    public boolean validateCard(Ticket ticket) {
+        // to do
+        return true;
+    }
+
+    // Process the payment, subtracting credit score from the ticket
+    public void paymentProcess(Ticket ticket, double price) {
+        // to do
+    }
+
+    // Getters
+    public int getId() {
+        return this.id;
+    }
+
+    public Bus(int id){
+        this.id = id;
     }
 }
--- src/main/java/busticket/Ticket.java
+++ src/main/java/busticket/Ticket.java
@@ -1,8 +1,25 @@
 package busticket;
 
 public class Ticket {
+    private int id;
+    private double credit;
+    private String createdAt;
+    private String user;
 
-    public static void main(String[] args) {
-        System.out.println("Ticket Test");
+    public void addCredit(double value) {
+        // to do
+    }
+
+    //Getters
+    public int getId() { return this.id; }
+    public double getCredit() { return this.credit; }
+    public String getCreatedAt() { return this.createdAt; }
+    public String getUser() { return this.user; }
+
+    public Ticket(int _id, String _user, String _createdAt) {
+        this.id = _id;
+        this.user = _user;
+        this.createdAt = _createdAt;
+        this.credit = 0.0;
     }
 }
